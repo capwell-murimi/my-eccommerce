69c8ad5b4490e3fdf41bdb7126f84951458f16d2
capwell-murimi <capwellmurimi@gmail.com>
2025-03-28 15:14:31 +0300

    Small fix

--- components/shared/header/search.tsx
+++ components/shared/header/search.tsx
@@ -8,39 +8,36 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
-
 import { APP_NAME } from '@/lib/constants'
 const categories = ['men', 'women', 'kids', 'accessories']
-
 export default async function Search() {
-  ;<form action='/search' method='get' className='flex items-stretch h-10'>
-    <Select name='category'>
-      <SelectTrigger className='w-auto h-full dark:border-gray-200 bg-gray-100 text-black border-r rounded-r-none rounded'>
-        <SelectValue placeholder='all' />
-      </SelectTrigger>
-
-      <SelectContent position='popper'>
-        <SelectItem value='all'>All</SelectItem>
-        {categories.map((category) => (
-          <SelectItem key={category} value={category}>
-            {category}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
-
-    <Input
-      className='flex-1 rounded-none dark:border-gray-200 bg-gray-100 text-black text-base h-full'
-      placeholder={`Search Site ${APP_NAME}`}
-      name='q'
-      type='search'
-    />
-
-    <button
-      type='submit'
-      className='bg-primary text-blak w-10  rounded-s-none rounded-e-md h-full px-3 py-2'
-    >
-      <SearchIcon className='w-6 h-6' />
-    </button>
-  </form>
+  return (
+    <form action='/search' method='GET' className='flex  items-stretch h-10 '>
+      <Select name='category'>
+        <SelectTrigger className='w-auto h-full dark:border-gray-200 bg-gray-100 text-black border-r  rounded-r-none rounded-l-md'>
+          <SelectValue placeholder='All' />
+        </SelectTrigger>
+        <SelectContent position='popper'>
+          <SelectItem value='all'>All</SelectItem>
+          {categories.map((category) => (
+            <SelectItem key={category} value={category}>
+              {category}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Input
+        className='flex-1 rounded-none dark:border-gray-200 bg-gray-100 text-black text-base h-full'
+        placeholder={`Search Site ${APP_NAME}`}
+        name='q'
+        type='search'
+      />
+      <button
+        type='submit'
+        className='bg-primary text-primary-foreground text-black rounded-s-none rounded-e-md h-full px-3 py-2 '
+      >
+        <SearchIcon className='w-6 h-6' />
+      </button>
+    </form>
+  )
 }
