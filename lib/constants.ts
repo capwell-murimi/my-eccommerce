88b495c8cc8be2fda2cf12ac2986269306035846
capwell-murimi <capwellmurimi@gmail.com>
2025-03-28 15:08:05 +0300

    Aded cool things

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'MyEccommerce'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'find first,buy fast'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'An amazon clone with nextJS and MongoDB'
